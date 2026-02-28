fed8840454b0389c34500d2a3322f461c96d6262
taydevijay <vijay@jenextech.com>
2026-02-28 17:28:23 +0530

    add validations in setting file

--- Server/src/middlewares/firmwareFileUpload.js
+++ Server/src/middlewares/firmwareFileUpload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
-const uploadDir = path.join(__dirname, "../uploads/ota");
+const uploadDir = path.join(__dirname, "../uploads");
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
